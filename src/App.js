5ebfd5287e827e6538853f872e49647a872d0a2c
manuelsanchez2 <manusansan22@gmail.com>
2020-08-12 13:29:53 +0200

    Add rainbow borders and improve the layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+// import List from './components/List';
 
 function App() {
   return (
-    <div>Pokedex</div>
+    <div className="app">
+      <header>
+        Pokedex <input />
+      </header>
+    
+      <main className="colorful-border">Pokemons
+      </main>
+      <footer>Footer</footer>
+    </div>
   );
 }
 
