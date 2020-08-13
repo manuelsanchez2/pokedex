c7f356b260e6f8de6b2505470df64323188be89a
manuelsanchez2 <manusansan22@gmail.com>
2020-08-13 11:41:26 +0200

    Add a button with a function

--- src/App.js
+++ src/App.js
@@ -4,52 +4,75 @@ import List from './components/List';
 import ListItem from './components/ListItem'
 import ListItemIcon from './components/ListItemIcon';
 import ListItemText from './components/ListItemText';
-import BulbasaurSrc from './assets/Bulbasaur.jpg'
-import PikachuSrc from './assets/Pikachu.jpg'
-import SnorlaxSrc from './assets/Snorlax.jpg'
-import ShoeSrc from './assets/Shoe.png'
+import PokeButton from './components/PokeButton';
+
+const bulbasaur = {
+  name: "Bulbasaur",
+  id: "#001",
+  imgSrc: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+  link: "#bulbasaur",
+}
+
+const ivysaur = {
+  name: "Ivysaur",
+  id: "#002",
+  imgSrc: "https://img.pokemondb.net/artwork/large/ivysaur.jpg",
+  link: "#ivysaur",
+}
+
+const venusaur = {
+  name: "Venusaur",
+  id: "#003",
+  imgSrc: "https://img.pokemondb.net/artwork/large/venusaur.jpg",
+  link: "#venusaur",
+}
+
+const charmander = {
+  name: "Charmander",
+  id: "#004",
+  imgSrc: "https://img.pokemondb.net/artwork/large/charmander.jpg",
+  link: "#charmander",
+}
+
+const charmeleon = {
+  name: "Charmeleon",
+  id: "#005",
+  imgSrc: "https://img.pokemondb.net/artwork/large/charmeleon.jpg",
+  link: "#charmeleon",
+}
+
+const charizard = {
+  name: "Charizard",
+  id: "#006",
+  imgSrc: "https://img.pokemondb.net/artwork/large/charizard.jpg",
+  link: "#charizard",
+}
+
+const allPokemons = [bulbasaur, ivysaur, venusaur, charmander, charmeleon, charizard];
+
+const showPokemons = () => {
+  alert("bulbasaur");
+}
+
+// const [pokemons, setPokemons] = React.useState([]);
 
 function App() {
   return (
     <div className="app">
       <header>
         POKEDEX <input placeholder="Searchi"/>
+        <PokeButton onClick={() => showPokemons()}>LOAD POKEMONS</PokeButton>
       </header>
     
       <main className="colorful-border">
         <List>
-          <ListItem href= "">
-
-            <ListItemIcon src= {BulbasaurSrc} alt="Bulbasaur"/>
-            <ListItemText primary="Bulbasaur" secondary="#001"/>
-      
-          </ListItem>
-          <ListItem href= "">
-
-            <ListItemIcon src= "https://img.pokemondb.net/artwork/large/ivysaur.jpg" alt="Ivysaur"/>
-            <ListItemText primary="Ivysaur" secondary="#002"/>
-
-          </ListItem>
-          <ListItem href= "ff">
-
-            <ListItemIcon src= {PikachuSrc} alt="Pikachu"/>
-            <ListItemText primary="Pikachu" secondary="#025"/>
-      
-          </ListItem>
-
-          <ListItem href= "ff">
-
-            <ListItemIcon src= {SnorlaxSrc} alt="Snorlax"/>
-            <ListItemText primary="Snorlax" secondary="#143"/>
-      
-          </ListItem>
-
-          <ListItem href= "ff">
-
-            <ListItemIcon src= {ShoeSrc} alt="Shoe"/>
-            <ListItemText primary="Shoe" secondary="#???"/>
-      
-          </ListItem>
+          {allPokemons.map((pokemon) => (
+            <ListItem ley={pokemon.id.toString()} href= {pokemon.link}>
+              <ListItemIcon src= {pokemon.imgSrc} alt={`Picture of ${pokemon.name}`}
+              />
+               <ListItemText primary={pokemon.name} secondary={pokemon.id}/>
+            </ListItem>
+          ))};
         </List>
       </main>
       <footer>Footer</footer>
