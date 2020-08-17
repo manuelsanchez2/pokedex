5cf7f5805b199348872f259d71ca244e5058d95b
manuelsanchez2 <manusansan22@gmail.com>
2020-08-17 09:50:43 +0200

    Add the Router components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import ListItemIcon from "./components/ListItemIcon";
 import ListItemText from "./components/ListItemText";
 import { fetchPokemons } from "./api/pokemon";
 import LoadingScreen from "./components/LoadingScreen";
+// Podemos importar funciones y cambiarle el nombre
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function waitFor(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
@@ -26,39 +34,55 @@ function App() {
     fetchData();
   }, []);
 
+  if (isLoading || pokemons === null) {
+    return <LoadingScreen />;
+  }
   const filteredPokemons = pokemons?.filter((pokemon) => {
     return pokemon.name.toLowerCase().match(query.toLowerCase());
   });
 
-  if (isLoading || pokemons === null) {
-    return <LoadingScreen />;
-  }
-
   return (
-    <div className="app">
-      <header>
-        POKEDEX{" "}
-        <input
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-          placeholder="Search your pokemon"
-        />
-      </header>
-      <main className="colorful-border">
-        <List>
-          {filteredPokemons?.map((pokemon) => (
-            <ListItem key={pokemon.id} href={pokemon.link}>
-              <ListItemIcon
-                src={pokemon.imgSrc}
-                alt={`Picture of ${pokemon.name}`}
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/pokemons">
+            <header>
+              POKEDEX{" "}
+              <input
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+                placeholder="Search your pokemon"
               />
-              <ListItemText primary={pokemon.name} secondary={pokemon.id} />
-            </ListItem>
-          ))}
-        </List>
-      </main>
-      <footer>Footer</footer>
-    </div>
+            </header>
+            <main className="colorful-border">
+              <List>
+                {filteredPokemons?.map((pokemon) => (
+                  <ListItem key={pokemon.id} href={pokemon.link}>
+                    <ListItemIcon
+                      src={pokemon.imgSrc}
+                      alt={`Picture of ${pokemon.name}`}
+                    />
+                    <ListItemText
+                      primary={pokemon.name}
+                      secondary={pokemon.id}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </main>
+          </Route>
+          <Route path="/items">ITEMS</Route>
+          <Route path="/">
+            <Redirect to="pokemons" />
+          </Route>
+        </Switch>
+        <footer>
+          <Link to="/pokemons">Pokemons</Link>
+          <Link to="/items">Items</Link>
+          <Link to="/home">Home</Link>
+        </footer>
+      </div>
+    </Router>
   );
 }
 
