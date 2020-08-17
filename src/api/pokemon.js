acb73edaaad3fbdfa85ca9c61050ad12094c0cde
manuelsanchez2 <manusansan22@gmail.com>
2020-08-17 12:01:33 +0200

    Display single pokemon page

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,15 +1,36 @@
 export async function fetchPokemons() {
-    const response = await fetch("https://unpkg.com/pokemons@1.1.0/pokemons.json");
-    if (!response.ok) {
-        throw new Error(response);
-    }
-    const results = await response.json();
-    const pokemons = results.results.map((pokemon) => ({
-        name: pokemon.name,
-        id: pokemon.national_number,
-        imgSrc: pokemon.sprites.animated,
-        link: `#${pokemon.name.toLowerCase()}`,
-    }));
-const uniquePokemons = pokemons.filter((pokemon, index) => pokemons.findIndex((other) => other.id === pokemon.id) === index)
-return uniquePokemons;
+  const response = await fetch(
+    "https://unpkg.com/pokemons@1.1.0/pokemons.json"
+  );
+  if (!response.ok) {
+    throw new Error(response);
+  }
+  const results = await response.json();
+  const pokemons = results.results.map((pokemon) => ({
+    name: pokemon.name,
+    id: pokemon.national_number,
+    imgSrc: pokemon.sprites.animated,
+    link: `#${pokemon.name.toLowerCase()}`,
+  }));
+  const uniquePokemons = pokemons.filter(
+    (pokemon, index) =>
+      pokemons.findIndex((other) => other.id === pokemon.id) === index
+  );
+  return uniquePokemons;
+}
+export async function fetchPokemon(pokemonName) {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+
+  if (!response.ok) {
+    throw new Error(response);
+  }
+  const result = await response.json();
+  const pokemon = {
+    name: result.name,
+    id: result.id,
+    imgSrc: result.sprites.front_shiny,
+  };
+  return pokemon;
 }
--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { fetchPokemon } from "../api/pokemon";
+import LoadingScreen from "../components/LoadingScreen";
 
-function Pokemon(props) {
+function Pokemon() {
   const { name } = useParams();
-  return <div>{name}</div>;
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(null);
+        const fetchedPokemon = await fetchPokemon(name);
+        setPokemon(fetchedPokemon);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      }
+    }
+    fetchData();
+  }, [name]);
+
+  if (error) {
+    return <div>ERROR!!!</div>;
+  }
+  if (loading || !pokemon) {
+    return <LoadingScreen />;
+  }
+  return (
+    <div>
+      <div>ID: {pokemon.id}</div>
+      <div>Name: {pokemon.name}</div>
+      <div>
+        IMG: <img src={pokemon.imgSrc} alt={pokemon.name} />
+      </div>
+    </div>
+  );
 }
 
 export default Pokemon;
